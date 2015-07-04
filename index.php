c11cf303ce5976330087958cd9567717cf4baf4e
DenchikBY <butko.denis.95@gmail.com>
2015-07-05 00:51:28 +0300

    __autoload to spl_autoload_register

--- index.php
+++ index.php
@@ -4,9 +4,8 @@ use System\Route;
 
 define('APP_PATH', __DIR__);
 
-function __autoload($path)
-{
-    include realpath(strtolower(__DIR__ . DIRECTORY_SEPARATOR . str_replace('\\', '/', $path) . '.php'));
-}
+spl_autoload_register(function ($class) {
+    include realpath(strtolower(__DIR__ . DIRECTORY_SEPARATOR . str_replace('\\', '/', $class) . '.php'));
+});
 
 Route::init();
