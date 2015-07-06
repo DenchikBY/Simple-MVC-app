c82aee6ed60e7be1b351323701dadb57d2355a1a
DenchikBY <butko.denis.95@gmail.com>
2015-07-07 00:57:38 +0300

    Added Config::set() method

--- system/config.php
+++ system/config.php
@@ -5,22 +5,49 @@ class Config
 
     private static $config;
 
-    public static function get($path)
+    private static function initialGet()
     {
         if (!self::$config) {
             self::$config = include APP_PATH . '/config.php';
         }
-        $path = explode('.', $path);
-        $return = &self::$config;
-        foreach ($path as $part) {
-            if (isset($return[$part])) {
-                $return = &$return[$part];
-            } else {
-                $return = null;
-                break;
+    }
+
+    public static function get($path = null)
+    {
+        self::initialGet();
+        if ($path == null) {
+            $return = self::$config;
+        } else {
+            $path = explode('.', $path);
+            $return = &self::$config;
+            foreach ($path as $part) {
+                if (isset($return[$part])) {
+                    $return = $return[$part];
+                } else {
+                    $return = null;
+                    break;
+                }
             }
         }
         return $return;
+    }
+
+    public static function set($path, $value)
+    {
+        self::initialGet();
+        $path = explode('.', $path);
+        $count = count($path);
+        $config = &self::$config;
+        for ($i = 1; $i <= $count; ++$i) {
+            if ($i < $count) {
+                if (!isset($config[$path[$i - 1]])) {
+                    $config[$path[$i - 1]] = [];
+                }
+                $config = &$config[$path[$i - 1]];
+            } else {
+                $config[$path[$i - 1]] = $value;
+            }
+        }
     }
 
 }
